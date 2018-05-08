969d2c8abfef7f2cf80ed6ef1f9a224110038c51
Ryan Hardin <ryan.hardin@nutanix.com>
2018-05-08 14:01:17 -0400

    Added email

--- clusterizer.py
+++ clusterizer.py
@@ -1,9 +1,16 @@
 import requests
+import subprocess
 import time
 
 CLOUDALIZER_IP = "35.196.167.141"
 CLUSTER_IP = "10.21.55.37"
 VM_UUID = "314cecaf-03cc-437d-9828-32e99fd474a9"
+
+
+def send_mail(address, sender, subject, body):
+  cmd = "echo \"%s\" | mail -v -s \"%s\" -r \"%s\" %s" % (
+    address, subject, sender, body)
+  subprocess.check_call(cmd, shell=True)
 
 
 def toggle_vm_power():
@@ -14,18 +21,26 @@ def toggle_vm_power():
     CLUSTER_IP, VM_UUID)
   response = s.get(url, verify=False)
   vm_info = response.json()
-  if vm_info.get("power_state", "").lower() == "on":
+
+  vm_is_on = vm_info.get("power_state", "").lower() == "on"
+  if vm_is_on:
+    current_state = "on"
+    transition = "off"
     print "VM is ON,  and will be powered OFF"
-    response = s.post(url + "/set_power_state",
-                      json={"transition": "OFF"},
-                      verify=False)
-    task_info = response.json()
   else:
-    print "VM is OFF, and nwill be powered ON"
-    response = s.post(url + "/set_power_state",
-                      json={"transition": "ON"},
-                      verify=False)
-    task_info = response.json()
+    current_state = "off"
+    transition = "on"
+
+  print "VM is %s, and will be powered %s" % (current_state, transition)
+  response = s.post(url + "/set_power_state",
+                    json={"transition": transition.upper()},
+                    verify=False)
+  task_info = response.json()
+  print "Created task '%s'" % task_info.get("task_uuid")
+  send_mail(address="7209332478@vtext.com",
+            sender="zero_cool@aol.com",
+            subject="Hey bro, your VM turned %s" % transition.upper(),
+            body="https://www.youtube.com/watch?v=wlMvYx11V-Y")
 
 
 if __name__ == '__main__':
